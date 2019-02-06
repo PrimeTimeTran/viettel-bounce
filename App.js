b3fb63505a111de9cef93b91ccf203eb7c23189f
Loi Tran <lvt15@my.fsu.edu>
2019-02-06 20:44:57 +0700

    Complete animation

--- App.js
+++ App.js
@@ -4,66 +4,121 @@ import {
   View,
   Alert,
   Modal,
+  Easing,
+  Animated,
   StyleSheet,
   TouchableHighlight,
 } from 'react-native'
 
-
 export default class App extends React.Component {
-  state = { modalVisible: false }
+  state = {
+    modalVisible: false,
+    animateYAxis: new Animated.Value(0)
+  }
 
   toggleModal = () => {
-    this.setState(({ modalVisible }) => {
-      return { modalVisible: !modalVisible }
+    this.setState({ modalVisible: true }, this.openAnimation)
+  }
+
+  closeModal = () => {
+    this.closeAnimation()
+  }
+
+  openAnimation = () => {
+    const { animateYAxis } = this.state
+
+    Animated.timing(animateYAxis, {
+      toValue: -100,
+      duration: 250,
+      easing: Easing.elastic(1.5),
+    }).start()
+  }
+
+  closeAnimation = () => {
+    const { animateYAxis } = this.state
+
+    Animated.timing(animateYAxis, {
+      toValue: 0,
+      duration: 250,
+      easing: Easing.bezier(0, -0.2, 0, -0.4),
+    }).start(() => {
+      this.setState({ modalVisible: false })
     })
   }
 
   render() {
+    const {
+      textStyle,
+      openStyle,
+      container,
+      modalStyle,
+      closeStyle,
+      littleButtons
+    } = styles
+
+    const { animateYAxis, modalVisible } = this.state
+
+    const animatedInfo = {
+      ...littleButtons,
+      backgroundColor: 'yellow',
+      transform: [{ translateY:  animateYAxis }]
+    }
+
+    const addAnimatedMap = Animated.multiply(1.5, animateYAxis)
+
+    const animatedMap = {
+      ...littleButtons,
+      backgroundColor: 'blue',
+      transform: [{ translateY:  addAnimatedMap }]
+    }
+
+    const addAnimatedPhone = Animated.multiply(2, animateYAxis)
+
+    const animatedPhone = {
+      ...littleButtons,
+      backgroundColor: 'pink',
+      transform: [{ translateY:  addAnimatedPhone }]
+    }
+
     return (
-      <View style={styles.container}>
-      <Text>Gogogo</Text>
+      <View style={container}>
+        <Text>Some background text</Text>
         <Modal
           transparent
           animationType="fade"
-          visible={this.state.modalVisible}
+          visible={modalVisible}
           onRequestClose={() => { Alert.alert('Modal has been closed.')}}>
-          <View
-            style={{
-              flex: 1,
-              alignItems: 'center',
-              justifyContent: 'center',
-              backgroundColor: 'rgba(0,0,0,0.5)'
-            }}
-          >
-            <TouchableHighlight
-              onPress={this.toggleModal}
-              style={{
-                right: 20,
-                bottom: 20,
-                width: 100,
-                height: 100,
-                borderRadius: 50,
-                position: 'absolute',
-                backgroundColor: 'red',
-                justifyContent: 'center'
-              }}>
-                <Text style={{ alignSelf: 'center' }}>Hide</Text>
+          <View style={modalStyle}>
+
+            <Animated.View style={[closeStyle, animatedPhone]}>
+              <TouchableHighlight onPress={this.toggleModal}>
+                  <Text style={textStyle}>Phone</Text>
               </TouchableHighlight>
+            </Animated.View>
+
+            <Animated.View style={[closeStyle, animatedMap]}>
+              <TouchableHighlight onPress={this.toggleModal}>
+                  <Text style={textStyle}>Maps</Text>
+              </TouchableHighlight>
+            </Animated.View>
+
+            <Animated.View style={[closeStyle, animatedInfo]}>
+              <TouchableHighlight onPress={this.toggleModal}>
+                  <Text style={textStyle}>Info</Text>
+              </TouchableHighlight>
+            </Animated.View>
+
+            <TouchableHighlight
+              style={closeStyle}
+              onPress={this.closeModal}>
+                <Text style={textStyle}>Hide</Text>
+            </TouchableHighlight>
           </View>
         </Modal>
         <TouchableHighlight
           onPress={this.toggleModal}
-          style={{
-            right: 20,
-            bottom: 20,
-            width: 100,
-            height: 100,
-            borderRadius: 50,
-            position: 'absolute',
-            backgroundColor: 'red',
-            justifyContent: 'center'
-          }}>
-          <Text style={{ alignSelf: 'center', justifyContent: 'center',  }}>Show Modal</Text>
+          style={openStyle}>
+          <Text style={textStyle}>Show Modal</Text>
         </TouchableHighlight>
       </View>
     )
@@ -77,4 +132,39 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    alignSelf: 'center'
+  },
+  openStyle: {
+    right: 20,
+    bottom: 20,
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    position: 'absolute',
+    backgroundColor: 'red',
+    justifyContent: 'center'
+  },
+  modalStyle: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  },
+  closeStyle: {
+    right: 20,
+    bottom: 20,
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    position: 'absolute',
+    backgroundColor: 'red',
+    justifyContent: 'center'
+  },
+  littleButtons: {
+    width: 50,
+    right: 40,
+    height: 50,
+    bottom: 40,
+  }
 })
